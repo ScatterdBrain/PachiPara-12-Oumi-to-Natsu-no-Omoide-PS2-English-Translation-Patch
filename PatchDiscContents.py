6d9cf5b4e72b9c9f57aa33a45d93b79734460486
ScatterdBrain <Internetactivity@yandex.ru>
2026-02-18 21:26:31 +0100

    Update PatchDiscContents.py

--- PatchDiscContents.py
+++ PatchDiscContents.py
@@ -2,10 +2,14 @@ import sys
 import os
 from python_scripts.ReinsertText import main as reinsert_text
 from python_scripts.ReinsertFont import main as reinsert_font
+from python_scripts.ReinsertGraphics import main as reinsert_graphics
+#from python_scripts.ApplyHacks import main as apply_hacks
 
 work_dir = os.getcwd()
 reinsert_text(work_dir)
 reinsert_font(work_dir)
+reinsert_graphics(work_dir)
+#apply_hacks(work_dir)
 
 input('Press Enter to close.')
 sys.exit()
