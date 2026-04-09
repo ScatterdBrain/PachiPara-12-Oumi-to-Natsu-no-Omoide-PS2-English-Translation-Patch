66f965edd8e3e1b59ed5926a6e7c8c4c19d73855
ScatterdBrain <Internetactivity@yandex.ru>
2026-04-09 16:21:12 +0200

    Update TM2toPNG.py

--- python_scripts/TM2toPNG.py
+++ python_scripts/TM2toPNG.py
@@ -1,138 +1,98 @@
 import sys
 import os
-import zlib
-
+import PNGHelp as png
 
 # TM2 specifications https://openkh.dev/common/tm2.html
-# PNG specifications https://www.w3.org/TR/PNG-Introduction.html
-PNG_SIGNATURE = b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A'
-IHDR = b'\x49\x48\x44\x52'
-PLTE = b'\x50\x4C\x54\x45'
-TRNS = b'\x74\x52\x4E\x53'
-IDAT = b'\x49\x44\x41\x54'
-IEND = b'\x49\x45\x4E\x44'
-BUFFER_SIZE = 16384
+# basic tm2 to png converter that only supports image color types 3, 4, 5
+# tm2 header is preserved in the png text chunk as a hex string
+
+def tm2_to_png(tm2_data : bytearray, half_trans : bool = True):
+    if tm2_data[:4] != bytearray(b'TIM2'):
+        return "not TM2 data"
+    color_size = int.from_bytes(tm2_data[20:24], 'little')
+    pixel_size = int.from_bytes(tm2_data[24:28], 'little')
+    match int.from_bytes(tm2_data[35:36], 'little'):
+        case 3:
+            bpp = 32
+            color_type = 6
+        case 4:
+            bpp = 4
+            color_type = 3
+        case 5:
+            bpp = 8
+            color_type = 3
+        case _:
+            return "bpp not supported"
+    width = int.from_bytes(tm2_data[36:38], 'little')
+    height = int.from_bytes(tm2_data[38:40], 'little')
+    pixel_data = bytearray(tm2_data[64:64 + pixel_size])
+    if color_type == 3:
+        color_data = bytearray(tm2_data[64 + pixel_size:])
+        if half_trans:
+            fix_halved_transparanceis(color_data)
+    elif color_type == 6 and half_trans:
+        fix_halved_transparanceis(pixel_data)
+    if bpp == 8 and color_type == 3:
+        color_data = unswizzle_palette(color_data)
+    if bpp == 4:
+        swap_4bit_nibbles(pixel_data)
+    png_data = bytearray()
+    png_data += png.PNG_SIGNATURE
+    png.write_ihdr(png_data, width, height, bpp if bpp != 32 else 8, color_type)
+    if 'color_data' in locals():
+        png.write_plte(png_data, color_data)
+        png.write_trns(png_data, color_data)
+    png.write_text(png_data, "TM2_HEADER", tm2_data[:64].hex())
+    png.write_idat(png_data, png.compress_pixel_data(png.filter_pixel_data(pixel_data, width, bpp)))
+    png.write_iend(png_data)
+    return png_data
+
+
+def fix_halved_transparanceis(data : bytearray):
+    for i in range(0, len(data), 4):
+        if data[i + 3] > 0 and data[i + 3] <= 128:
+            data[i + 3] = (data[i + 3] << 1) - 1
+    return
+
+
+def unswizzle_palette(color_data : bytearray):
+    sorted_palette = bytearray()
+    for i in range(0, 1024, 128):
+        chunk_1 = color_data[i:i + 32]
+        chunk_2 = color_data[i + 32:i + 64]
+        chunk_3 = color_data[i + 64:i + 96]
+        chunk_4 = color_data[i + 96:i + 128]
+        sorted_palette += chunk_1 + chunk_3 + chunk_2 + chunk_4
+    return sorted_palette
+
+
+def swap_4bit_nibbles(pixel_data : bytearray):
+    for i in range(len(pixel_data)):
+        pixel_data[i] = ((pixel_data[i] << 4) & 255) | ((pixel_data[i] >> 4) % 255)
 
 
 def main():
     if len(sys.argv) == 1:
-        print("Drop TIM2.")
+        print("Drop TIM2 on the py file.")
         input('Press Enter to close.')
         sys.exit()
     else:
         for arg in sys.argv[1:]:
             try:
-                infile = open(arg, 'rb')
+                raw_data = open(arg, 'rb').read()
             except:
                 print(os.path.abspath(arg) + " failed to open.")
                 continue
-            if infile.read(4) != b'TIM2':
-                print(os.path.abspath(arg) + " not TIM2.")
-                infile.close()
+            png_data = tm2_to_png(raw_data)
+            if type(png_data) is str:
+                print(os.path.abspath(arg) + " ERROR: " + png_data)
                 continue
-            infile.seek(20)
-            color_size = int.from_bytes(infile.read(4), byteorder='little', signed=False)
-            pixel_size = int.from_bytes(infile.read(4), byteorder='little', signed=False)
-            infile.seek(35)
-            match int.from_bytes(infile.read(1), byteorder='little', signed=False):
-                case 3:
-                    bpp = 32
-                case 4:
-                    bpp = 4
-                case 5:
-                    bpp = 8
-                case _:
-                    print(arg + " not supported.")
-                    infile.close()
-                    continue
-            width = int.from_bytes(infile.read(2), byteorder='little', signed=False)
-            height = int.from_bytes(infile.read(2), byteorder='little', signed=False)
-            infile.seek(64)
-            pixel_data = bytearray(infile.read(pixel_size))
-            # Fix transparencies.
-            if color_size != 0:
-                color_data = bytearray(infile.read(color_size))
-                for i in range(0, len(color_data), 4):
-                    if color_data[i + 3] > 0 and color_data[i + 3] <= 128:
-                        color_data[i + 3] = (color_data[i + 3] << 1) - 1
-            elif color_size == 0 and bpp == 32:
-                for i in range(0, len(pixel_data), 4):
-                    if pixel_data[i + 3] > 0 and pixel_data[i + 3] <= 128:
-                        pixel_data[i + 3] = (pixel_data[i + 3] << 1) - 1
-            if bpp == 8:
-                # Unswizzle palette.
-                sorted_palette = bytearray()
-                for i in range(0, 1024, 128):
-                    chunk_1 = color_data[i:i + 32]
-                    chunk_2 = color_data[i + 32:i + 64]
-                    chunk_3 = color_data[i + 64:i + 96]
-                    chunk_4 = color_data[i + 96:i + 128]
-                    sorted_palette += chunk_1 + chunk_3 + chunk_2 + chunk_4
-                color_data = sorted_palette
-                del sorted_palette
-            if bpp == 4:
-                # Swap 4bit nibles.
-                for i in range(len(pixel_data)):
-                    pixel_data[i] = ((pixel_data[i] << 4) & 255) | ((pixel_data[i] >> 4) % 255)
-            infile.close()
-            # Convert to PNG data.
-            del color_size
-            del pixel_size
-            png_data = bytearray()
-            buffer = bytearray()
-            png_data += PNG_SIGNATURE
-            buffer += (width).to_bytes(4, byteorder='big', signed=False)
-            buffer += (height).to_bytes(4, byteorder='big', signed=False)
-            match bpp:
-                case 4 | 8:
-                    buffer += (bpp).to_bytes(1, byteorder='big', signed=False)
-                    buffer += b'\x03\x00\x00\x00'
-                case 32:
-                    buffer += (8).to_bytes(1, byteorder='big', signed=False)
-                    buffer += b'\x06\x00\x00\x00'
-            png_data += len(buffer).to_bytes(4, byteorder='big', signed=False)
-            png_data += IHDR + buffer + zlib.crc32(IHDR + buffer).to_bytes(4, byteorder='big', signed=False)
-            if 'color_data' in locals():
-                plte_chunk = bytearray()
-                trns_chunk = bytearray()
-                for i in range(0, len(color_data), 4):
-                    plte_chunk += color_data[i:i + 3]
-                    trns_chunk += color_data[i + 3].to_bytes(1)
-                buffer = plte_chunk
-                del plte_chunk
-                png_data += len(buffer).to_bytes(4, byteorder='big', signed=False)
-                png_data += PLTE + buffer + zlib.crc32(PLTE + buffer).to_bytes(4, byteorder='big', signed=False)
-                buffer = trns_chunk
-                del trns_chunk
-                png_data += len(buffer).to_bytes(4, byteorder='big', signed=False)
-                png_data += TRNS + buffer + zlib.crc32(TRNS + buffer).to_bytes(4, byteorder='big', signed=False)
-            filtered_pixel_data = bytearray()
-            scanline = bpp * width // 8
-            for i in range(0, len(pixel_data), scanline):
-                filtered_pixel_data += (0).to_bytes() + pixel_data[i: i + scanline]
-            del pixel_data
-            comp_buff = zlib.compress((filtered_pixel_data), level=6, wbits=15)
-            del scanline
-            del filtered_pixel_data
-            pointer = 0
-            loop = True
-            while loop:
-                if pointer + BUFFER_SIZE >= len(comp_buff):
-                    buffer = comp_buff[pointer:]
-                    loop = False
-                else:
-                    buffer = comp_buff[pointer:pointer + BUFFER_SIZE]
-                png_data += len(buffer).to_bytes(4, byteorder='big', signed=False)
-                png_data += IDAT + buffer + zlib.crc32(IDAT + buffer).to_bytes(4, byteorder='big', signed=False)
-                pointer += BUFFER_SIZE
-            png_data += (0).to_bytes(4, byteorder='big', signed=False)
-            png_data += IEND + zlib.crc32(IEND).to_bytes(4, byteorder='big', signed=False)
-            with open(arg + ".png", 'wb') as outfile:
-                outfile.write(png_data)
-                print(os.path.abspath(arg + ".png") + " done.")
+            else:
+                with open(arg + ".png", 'wb') as outfile:
+                    outfile.write(png_data)
+                    print(os.path.abspath(arg + ".png") + " done.")
     input('Press Enter to close.')
 
 
 if __name__ == '__main__':
     main()
-
